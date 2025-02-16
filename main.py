67193c6fd89450043a079e6ee57bde545e3ba2a9
KageRyo <k6.developer@gmail.com>
2025-02-16 14:31:35 +0800

    feat: test api develop

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ async def root():
     return {
         "message": "Hello FastAPI!"
     }
+    
+@app.get("/test/{username}")
+async def username_test(username):
+    return {
+        "message": f"Hello {username}!"
+    }
+    
+@app.post("/test/message")
+async def message_test(username, message):
+    return {
+        "message": 
+            f"Hello {username}! Your message is {message}"
+    }
