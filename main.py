dc5a961f4cf9e6108e05d7de1f45b6fa838beb52
KageRyo <k6.developer@gmail.com>
2025-03-09 14:55:53 +0800

    feat: json data handle

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+import pandas as pd
+from fastapi import FastAPI, HTTPException
 from src.schemas import CreateMessageSchema, MessageSchema
 
 app = FastAPI()
 
+# JSON 資料存儲
+def load_message():
+    try:
+        df = pd.read_json("message.json")
+        return df.to_dict(orient="records")
+    except FileNotFoundError:
+        return []
 
+def save_message(message):
+    df = pd.DataFrame(message)
+    df.to_json("message.json", orient="records")
+
+# 測試程式碼
 @app.get("/")
 async def root():
     return {
